aca107a7380870c281b7a3cd75aed171154ea0c9
kirill <kiruha3@mail.ru>
2023-08-16 14:27:01 +0700

    Финальная реализация

--- src/main/java/kiruha/com/calculator/CalculateService.java
+++ src/main/java/kiruha/com/calculator/CalculateService.java
@@ -11,39 +11,39 @@ public class CalculateService implements CalculateSirviceInterface {
                 "body{text-align: center;'}" +
                 "</style>" +
                 "<div class='header' >Выберите действие</div>" +
-                "<div style='display:inline-flex;' >" +
-                "<div class='columns'>" +
-                "<a >Сложение     </a>" +
-                "<br> <a href= 'calculator/plus?num1=5&num2=8'>5+8 </a> " +
-                "<br> <a href= 'calculator/plus?num1=12&num2=6'>12+6 </a> " +
-                "<br> <a href= 'calculator/plus?num1=7&num2=4'>7+4 </a> " +
-                "<br> <a href= 'calculator/plus?num1=4.6&num2=4'>4.6+4 </a>" +
-                "<br>" +
-                "</div>" +
-                "<div class='columns'>" +
-                "<a >Вычитание     </a>" +
-                "<br> <a href= 'calculator/minus?num1=5&num2=8'>5-8 </a> " +
-                "<br> <a href= 'calculator/minus?num1=12&num2=6'>12-6 </a> " +
-                "<br> <a href= 'calculator/minus?num1=7&num2=4'>7-4 </a> " +
-                "<br> <a href= 'calculator/minus?num1=4.6&num2=4'>4.6-4 </a>" +
-                "<br>" +
-                "</div>" +
-                "<div class='columns'>" +
-                "<a >Деление     </a>" +
-                "<br> <a href= 'calculator/div?num1=5&num2=8'>5/8 </a> " +
-                "<br> <a href= 'calculator/div?num1=12&num2=6'>12/6 </a> " +
-                "<br> <a href= 'calculator/div?num1=7&num2=4'>7/4 </a> " +
-                "<br> <a href= 'calculator/div?num1=4.6&num2=4'>4.6/4 </a>" +
-                "<br>" +
-                "</div>" +
-                "<div class='columns'>" +
-                "<a >Умножение     </a>" +
-                "<br> <a href= 'calculator/mult?num1=5&num2=8'>5*8 </a> " +
-                "<br> <a href= 'calculator/mult?num1=12&num2=6'>12*6 </a> " +
-                "<br> <a href= 'calculator/mult?num1=7&num2=4'>7*4 </a> " +
-                "<br> <a href= 'calculator/mult?num1=4.6&num2=4'>4.6*4 </a>" +
-                "<br>" +
-                "</div>" +
+                    "<div style='display:inline-flex;' >" +
+                    "<div class='columns'>" +
+                        "<a >Сложение     </a>" +
+                        "<br> <a href= 'calculator/plus?num1=5&num2=8'>5+8 </a> " +
+                        "<br> <a href= 'calculator/plus?num1=12&num2=6'>12+6 </a> " +
+                        "<br> <a href= 'calculator/plus?num1=7&num2=4'>7+4 </a> " +
+                        "<br> <a href= 'calculator/plus?num1=4.6&num2=4'>4.6+4 </a>" +
+                        "<br>" +
+                    "</div>" +
+                    "<div class='columns'>" +
+                        "<a >Вычитание     </a>" +
+                        "<br> <a href= 'calculator/minus?num1=5&num2=8'>5-8 </a> " +
+                        "<br> <a href= 'calculator/minus?num1=12&num2=6'>12-6 </a> " +
+                        "<br> <a href= 'calculator/minus?num1=7&num2=4'>7-4 </a> " +
+                        "<br> <a href= 'calculator/minus?num1=4.6&num2=4'>4.6-4 </a>" +
+                        "<br>" +
+                    "</div>" +
+                    "<div class='columns'>" +
+                        "<a >Деление     </a>" +
+                        "<br> <a href= 'calculator/div?num1=5&num2=8'>5/8 </a> " +
+                        "<br> <a href= 'calculator/div?num1=12&num2=6'>12/6 </a> " +
+                        "<br> <a href= 'calculator/div?num1=7&num2=4'>7/4 </a> " +
+                        "<br> <a href= 'calculator/div?num1=4.6&num2=4'>4.6/4 </a>" +
+                        "<br>" +
+                    "</div>" +
+                    "<div class='columns'>" +
+                        "<a >Умножение     </a>" +
+                        "<br> <a href= 'calculator/mult?num1=5&num2=8'>5*8 </a> " +
+                        "<br> <a href= 'calculator/mult?num1=12&num2=6'>12*6 </a> " +
+                        "<br> <a href= 'calculator/mult?num1=7&num2=4'>7*4 </a> " +
+                        "<br> <a href= 'calculator/mult?num1=4.6&num2=4'>4.6*4 </a>" +
+                        "<br>" +
+                    "</div>" +
                 "</div>";
     }
 
